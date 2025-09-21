c2e60622e8779c25f44c3593519ab27a81ce30eb
sbansal05 <siddhi.bansal0504@gmail.com>
2025-09-22 00:03:41 +0530

    Added v1 progress

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,94 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-
-
+import { UserModel, ContentModel } from "./db.js";
+import { userMiddleware } from "./middleware.js"
+import { JWT_PASSWORD } from "./config.js"
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+//zod validation
+    const username = req.body.username;
+    const password = req.body.password;
+//hash password
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        })
+
+        res.json({
+            message: "User signed up"
+        })
+
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
 
 })
 
-app.post("./api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser) {
+        const token  = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 
 })
 
-app.get("/api/v1/content", (req, res) => {
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link  = req.body.link;
+    const type = req.body.type;
+    await ContentModel.create({
+        link,
+        type,
+        //@ts-ignore
+        userId: req.userId,
+        tags: []
+    })
+
+    res.json({
+        message: "Content added"
+    })
 
 })
-app.delete("/api/v1/content", (req, res) => {
+app.get("/api/v1/content", async (req, res) => {
+    //@ts-ignore
+    const userId= req.userId;
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
 
+})
+
+app.delete("/api/v1/content",userMiddleware, async (req,res) => {
+    const contentId = req.body.contentId;
+
+    await ContentModel.deleteMany({
+        contentId,
+        //@ts-ignore
+        userId: req.userId
+    })
 })
 
 app.post("/api/vi/brain/share", (req, res) => {
@@ -27,4 +98,4 @@ app.get("/api/v1/brain/:shareLink", (req, res) => {
 
 })
 
-app.get
+app.listen(3001);
